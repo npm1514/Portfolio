b67ae37d25e861f7bec439084968b256904824bf
Nick Marucci <npm1514@gmail.com>
2016-04-25 14:21:10 -0600

    updated with new images and removed globe

--- app.js
+++ app.js
@@ -1,57 +1,15 @@
 var app = angular.module("portfolio", ['ui.router'])
-.directive('body', function() {
- return {
-   restrict: 'E',
-   scope: {
-     zoomin: '=',
-     zoomout: '='
-   }
- };
-})
-.directive('earth', function() {
- return {
-   restrict: 'E',
-   scope: {
-     zoomin: '=',
-     zoomout: '=',
-     cohortupdate:'='
-   },
-   controller: 'earthCtrl'
- };
-})
-.directive('bars', function() {
- return {
-  restrict: 'E',
-  scope: {
-    data:'=',
-    cohortupdate:'=',
-  },
-  controller: 'barsCtrl'
- };
-})
 .directive('splat', function() {
   return {
     restrict: 'E',
     controller: 'splatCtrl'
   };
-})
-.directive('scrollbar', function() {
- return {
-   restrict: 'E',
-   scope: {
-     cohortupdate: '=',
-     arcit: '&',
-     viewdate: '=',
-     play: '='
-   },
-   controller: 'scrollbarCtrl'
- };
 });
 
 $(document).ready(function() {
 
   $('#fullpage').fullpage({
-    anchors: ['view1', 'view2', 'view3', 'view4', 'view5', 'view6', 'view7'],
+    anchors: ['view1', 'view2', 'view3', 'view4', 'view5', 'view6', 'view7', 'view8'],
     menu: '#menu',
     css3: true,
     fitToSection: true,
