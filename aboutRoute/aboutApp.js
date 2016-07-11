ad27295defedc87f243bc3dbd40e0f9745ca6059
Nick Marucci <npm1514@gmail.com>
2016-07-11 16:27:20 -0600

    update to resume page

--- aboutRoute/aboutApp.js
+++ aboutRoute/aboutApp.js
@@ -42,33 +42,42 @@ app.controller("emailCtrl", function($scope, emailServ) {
   };
 });
 
+app.directive('emailDir',function(){
+  return {
+    restrict: 'E',
+    controller: 'emailCtrl',
+    template: '<div class="emailResponse"><h1>{{responseMessage}}</h1></div>'
+  };
+});
+
+
 
 app.service("emailServ", function($http) {
 
   this.sendEmail = function(name, email, message) {
     if (name && email && message) {
       return $http({
-          method: 'POST',
-          url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-          data: {
-            'key': 'kLu16ufqZK6L8u1zYNyj6Q',
-            'message': {
-              'from_email': 'npmarucci@sol-innovations.com',
-              'to': [{
-                'email': 'npmarucci@sol-innovations.com',
-                'name': 'Sol Innovations',
-                'type': 'to'
+          "method": "POST",
+          "url": "https://mandrillapp.com/api/1.0/messages/send.json",
+          "data": {
+            "key": "m9WhwvP-pCZrAOmfEmMceg",
+            "message": {
+              "from_email": "npmarucci@sol-innovations.com",
+              "to": [{
+                "email": "npmarucci@sol-innovations.com",
+                "name": "Sol Innovations",
+                "type": "to"
               }],
-              'autotext': 'true',
-              'subject': 'Customer Inquiry - ' + name + " @ " + email,
-              'text': message
+              "autotext": true,
+              "subject": "Customer Inquiry - " + name + " @ " + email,
+              "text": message
             }
           }
         })
         .then(function itWorked(response) {
-          return "Your email has been received. We will get back to you within the next 48 hours.";
+          return "Your email has been received. I will get back to you within the next 48 hours.";
         }, function itDidntWork(response){
-          return "Woops! Something went wrong. Please contact us via email or give us a call.";
+          return "Whoops! Something went wrong. Please contact me via email or give me a call.";
         });
     }
   };
